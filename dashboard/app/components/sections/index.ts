8a0fc724205fa3de974ae666b5f6b7fdde6449df
shadvls <yanshaaa28@gmail.com>
2026-02-25 12:00:00 +0000

    refactor: add barrel exports for cleaner component imports

---
+++ dashboard/app/components/sections/index.ts
@@ -0,0 +1,3 @@
+export { default as DashboardHeader } from "./DashboardHeader";
+export { default as SensorCard } from "./SensorCard";
+export { default as ScheduleCard } from "./ScheduleCard";
---
+++ dashboard/app/components/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as ClientProvider } from "./ClientProvider";
+export { default as Preloader } from "./Preloader";
+export { default as BackgroundGradient } from "./BackgroundGradient";
+export { default as OverlaySystem } from "./OverlaySystem";
